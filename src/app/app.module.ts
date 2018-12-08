d0fdeee81d379cac6dc3c0be1e56f1d62b8e03be
avinashk29 <akasliwal1998@gmail.com>
2018-12-08 14:30:55 +0530

    folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SignupComponent } from './Auth/signup/signup.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { ForgetPasswordComponent } from './Auth/forget-password/forget-password.component';
+import {AuthModuleModule} from '../app/Auth/auth-module/auth-module.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    ForgetPasswordComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModuleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
