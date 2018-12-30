1d94fae3f7abfdd845417e72ebe9bf693cb756fe
avinashk29 <akasliwal1998@gmail.com>
2018-12-30 15:14:16 +0530

    bpage

--- src/app/Bpage/b-page/b-page.component.ts
+++ src/app/Bpage/b-page/b-page.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./b-page.component.css']
 })
 export class BPageComponent implements OnInit {
-
+one = true;
+two = false;
+three = false;
+four = false;
   constructor() { }
 
   ngOnInit() {
   }
-
+  showTwo() {
+    this.one = false;
+    this.two = true;
+    this.three = false;
+    this.four = false;
+  }
+  showThree() {
+    this.one = false;
+    this.two = false;
+    this.three = true;
+    this.four = false;
+  }
+  showFour() {
+    this.one = false;
+    this.two = false;
+    this.three = false;
+    this.four = true;
+  }
 }
