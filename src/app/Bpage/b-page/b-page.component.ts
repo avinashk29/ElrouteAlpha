1334ed009a370a61c3ae022bd1d775e48da68704
avinashk29 <akasliwal1998@gmail.com>
2019-01-02 18:09:17 +0530

    bpage

--- src/app/Bpage/b-page/b-page.component.ts
+++ src/app/Bpage/b-page/b-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Event } from '@angular/router';
 
 @Component({
   selector: 'app-b-page',
@@ -6,11 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./b-page.component.css']
 })
 export class BPageComponent implements OnInit {
+
 one = true;
 two = false;
 three = false;
 four = false;
 items;
+ expand = [];
+
+groups;
   constructor() {
     this.items =  [
         {name: 'https://picsum.photos/200/300'},
@@ -34,9 +39,444 @@ items;
         {name: 'https://picsum.photos/200/300/?blur'},
         {name: 'https://picsum.photos/200/300/?random'}
     ];
+    this.groups = [
+     {
+      card : [
+        {
+      type: 1,
+        catogory: 'Laptp',
+        productName: 'Product Name',
+        company: 'Company ka Naam',
+        device: 'computer',
+        country: 'China',
+        discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+
+        },
+          {
+            type: 1,
+            catogory: 'Lap',
+            productName: 'Prot Name',
+            company: 'Company',
+            device: 'computer',
+            country: 'China',
+            discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+            },
+            {
+              type: 1,
+              catogory: 'Lap',
+              productName: 'Prot Name',
+              company: 'Company',
+              device: 'computer',
+              country: 'China',
+              discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+              },
+              {
+                type: 1,
+                catogory: 'Lap',
+                productName: 'Prot Name',
+                company: 'Company',
+                device: 'computer',
+                country: 'China',
+                discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                },
+                {
+                  type: 1,
+                  catogory: 'Lap',
+                  productName: 'Prot Name',
+                  company: 'Company',
+                  device: 'computer',
+                  country: 'China',
+                  discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                  },
+                  {
+                    type: 1,
+                    catogory: 'Lap',
+                    productName: 'Prot Name',
+                    company: 'Company',
+                    device: 'computer',
+                    country: 'China',
+                    discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                    },
+                    {
+                      type: 1,
+                      catogory: 'Lap',
+                      productName: 'Prot Name',
+                      company: 'Company',
+                      device: 'computer',
+                      country: 'China',
+                      discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                      },
+                      {
+                        type: 1,
+                        catogory: 'Lap',
+                        productName: 'Prot Name',
+                        company: 'Company',
+                        device: 'computer',
+                        country: 'China',
+                        discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                        }
+
+           ]
+     },
+
+     {
+      card : [
+        {
+      type: 1,
+        catogory: 'Laptp',
+        productName: 'Product Name',
+        company: 'Company ka Naam',
+        device: 'computer',
+        country: 'China',
+        discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+
+        },
+          {
+            type: 1,
+            catogory: 'Lap',
+            productName: 'Prot Name',
+            company: 'Company',
+            device: 'computer',
+            country: 'China',
+            discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+            },
+            {
+              type: 1,
+              catogory: 'Lap',
+              productName: 'Prot Name',
+              company: 'Company',
+              device: 'computer',
+              country: 'China',
+              discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+              },
+              {
+                type: 1,
+                catogory: 'Lap',
+                productName: 'Prot Name',
+                company: 'Company',
+                device: 'computer',
+                country: 'China',
+                discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                },
+                {
+                  type: 1,
+                  catogory: 'Lap',
+                  productName: 'Prot Name',
+                  company: 'Company',
+                  device: 'computer',
+                  country: 'China',
+                  discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                  },
+                  {
+                    type: 1,
+                    catogory: 'Lap',
+                    productName: 'Prot Name',
+                    company: 'Company',
+                    device: 'computer',
+                    country: 'China',
+                    discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                    },
+                    {
+                      type: 1,
+                      catogory: 'Lap',
+                      productName: 'Prot Name',
+                      company: 'Company',
+                      device: 'computer',
+                      country: 'China',
+                      discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                      },
+                      {
+                        type: 1,
+                        catogory: 'Lap',
+                        productName: 'Prot Name',
+                        company: 'Company',
+                        device: 'computer',
+                        country: 'China',
+                        discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                        }
+
+           ]
+     },
+
+     {
+      card : [
+        {
+      type: 1,
+        catogory: 'Laptp',
+        productName: 'Product Name',
+        company: 'Company ka Naam',
+        device: 'computer',
+        country: 'China',
+        discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+
+        },
+          {
+            type: 1,
+            catogory: 'Lap',
+            productName: 'Prot Name',
+            company: 'Company',
+            device: 'computer',
+            country: 'China',
+            discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+            },
+            {
+              type: 1,
+              catogory: 'Lap',
+              productName: 'Prot Name',
+              company: 'Company',
+              device: 'computer',
+              country: 'China',
+              discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+              },
+              {
+                type: 1,
+                catogory: 'Lap',
+                productName: 'Prot Name',
+                company: 'Company',
+                device: 'computer',
+                country: 'China',
+                discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                },
+                {
+                  type: 1,
+                  catogory: 'Lap',
+                  productName: 'Prot Name',
+                  company: 'Company',
+                  device: 'computer',
+                  country: 'China',
+                  discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                  },
+                  {
+                    type: 1,
+                    catogory: 'Lap',
+                    productName: 'Prot Name',
+                    company: 'Company',
+                    device: 'computer',
+                    country: 'China',
+                    discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                    },
+                    {
+                      type: 1,
+                      catogory: 'Lap',
+                      productName: 'Prot Name',
+                      company: 'Company',
+                      device: 'computer',
+                      country: 'China',
+                      discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                      },
+                      {
+                        type: 1,
+                        catogory: 'Lap',
+                        productName: 'Prot Name',
+                        company: 'Company',
+                        device: 'computer',
+                        country: 'China',
+                        discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                        }
+
+           ]
+     },
+
+     {
+      card : [
+        {
+      type: 1,
+        catogory: 'Laptp',
+        productName: 'Product Name',
+        company: 'Company ka Naam',
+        device: 'computer',
+        country: 'China',
+        discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+
+        },
+          {
+            type: 1,
+            catogory: 'Lap',
+            productName: 'Prot Name',
+            company: 'Company',
+            device: 'computer',
+            country: 'China',
+            discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+            },
+            {
+              type: 1,
+              catogory: 'Lap',
+              productName: 'Prot Name',
+              company: 'Company',
+              device: 'computer',
+              country: 'China',
+              discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+              },
+              {
+                type: 1,
+                catogory: 'Lap',
+                productName: 'Prot Name',
+                company: 'Company',
+                device: 'computer',
+                country: 'China',
+                discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                },
+                {
+                  type: 1,
+                  catogory: 'Lap',
+                  productName: 'Prot Name',
+                  company: 'Company',
+                  device: 'computer',
+                  country: 'China',
+                  discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                  },
+                  {
+                    type: 1,
+                    catogory: 'Lap',
+                    productName: 'Prot Name',
+                    company: 'Company',
+                    device: 'computer',
+                    country: 'China',
+                    discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                    },
+                    {
+                      type: 1,
+                      catogory: 'Lap',
+                      productName: 'Prot Name',
+                      company: 'Company',
+                      device: 'computer',
+                      country: 'China',
+                      discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                      },
+                      {
+                        type: 1,
+                        catogory: 'Lap',
+                        productName: 'Prot Name',
+                        company: 'Company',
+                        device: 'computer',
+                        country: 'China',
+                        discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                        }
+
+           ]
+     },
+
+     {
+      card : [
+        {
+      type: 1,
+        catogory: 'Laptp',
+        productName: 'Product Name',
+        company: 'Company ka Naam',
+        device: 'computer',
+        country: 'China',
+        discription: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+
+        },
+          {
+            type: 1,
+            catogory: 'Lap',
+            productName: 'Prot Name',
+            company: 'Company',
+            device: 'computer',
+            country: 'China',
+            discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+            },
+            {
+              type: 1,
+              catogory: 'Lap',
+              productName: 'Prot Name',
+              company: 'Company',
+              device: 'computer',
+              country: 'China',
+              discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+              },
+              {
+                type: 1,
+                catogory: 'Lap',
+                productName: 'Prot Name',
+                company: 'Company',
+                device: 'computer',
+                country: 'China',
+                discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                },
+                {
+                  type: 1,
+                  catogory: 'Lap',
+                  productName: 'Prot Name',
+                  company: 'Company',
+                  device: 'computer',
+                  country: 'China',
+                  discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                  },
+                  {
+                    type: 1,
+                    catogory: 'Lap',
+                    productName: 'Prot Name',
+                    company: 'Company',
+                    device: 'computer',
+                    country: 'China',
+                    discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                    },
+                    {
+                      type: 1,
+                      catogory: 'Lap',
+                      productName: 'Prot Name',
+                      company: 'Company',
+                      device: 'computer',
+                      country: 'China',
+                      discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                      },
+                      {
+                        type: 1,
+                        catogory: 'Lap',
+                        productName: 'Prot Name',
+                        company: 'Company',
+                        device: 'computer',
+                        country: 'China',
+                        discription: 'It is a long established fact that a reader will be d of a page when looking at its layout.'
+
+                        }
+
+           ]
+     }
+  ];
   }
 
   ngOnInit() {
+    for (let i = 0; i < this.groups.length; i++) {
+      this.expand[i] = false;
+      console.log(this.expand[i]);
+    }
   }
   showTwo() {
     this.one = false;
@@ -56,4 +496,14 @@ items;
     this.three = false;
     this.four = true;
   }
+
+  onExpand(i) {
+    console.log(i);
+    if (this.expand[i] === true) {
+             this.expand[i] = false;
+     } else {
+      this.expand[i] = true;
+    }
+  }
 }
+
