2dce0cd20d58a68fcc879657689fc3b2660a1244
avinashk29 <akasliwal1998@gmail.com>
2019-01-07 16:36:44 +0530

    company page

--- src/app/Bpage/b-page/b-page.component.ts
+++ src/app/Bpage/b-page/b-page.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Inject } from '@angular/core';
 import { Event } from '@angular/router';
+import {CompanyServiceService} from '../../Service/company-service.service';
+import {LOCAL_STORAGE, WebStorageService} from 'angular-webstorage-service';
 
 @Component({
   selector: 'app-b-page',
@@ -7,18 +9,24 @@ import { Event } from '@angular/router';
   styleUrls: ['./b-page.component.css']
 })
 export class BPageComponent implements OnInit {
-  getUrl() {
-    return "'url('assets/images/bk1.jpg'),linear-gradient(to right, gold 30%, transparent 80%)'"
-  }
+
 one = true;
 two = false;
 three = false;
 four = false;
 items;
  expand = [];
-
+ token;
 groups;
-  constructor() {
+name;
+country;
+city;
+industry;
+category;
+email;
+  constructor(@Inject (LOCAL_STORAGE) private storage: WebStorageService, public companyService: CompanyServiceService) {
+    this.companyService.token = this.storage.get('token');
+    this.token = this.storage.get('token');
     this.items =  [
         {name: 'https://picsum.photos/200/300'},
         {name: 'https://picsum.photos/g/200/300'},
@@ -479,6 +487,22 @@ groups;
       this.expand[i] = false;
       console.log(this.expand[i]);
     }
+this.companyService.GetCompany().subscribe(res => {
+  console.log(JSON.parse(res['_body'])[0].companyName);
+  this.storage.set('companyName',  JSON.parse(res['_body'])[0].companyName);
+  this.storage.set('country',  JSON.parse(res['_body'])[0].country);
+  this.storage.set('city',  JSON.parse(res['_body'])[0].city);
+  this.storage.set('companyEmail',  JSON.parse(res['_body'])[0].companyEmail);
+  this.storage.set('industry',  JSON.parse(res['_body'])[0].industry);
+  this.storage.set('category',  JSON.parse(res['_body'])[0].category);
+  this.name = this.storage.get('companyName');
+  this.country = this.storage.get('country');
+  this.city = this.storage.get('city');
+  this.email = this.storage.get('companyEmail');
+  this.industry = this.storage.get('industry');
+  this.category = this.storage.get('category');
+
+});
   }
   showTwo() {
     this.one = false;
