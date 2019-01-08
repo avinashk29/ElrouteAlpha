fa1a05e65d8e1d7047aaf2a38bb8e6987451478d
Arjun4396 <arjunkumar4396@gmail.com>
2019-01-08 18:20:24 +0530

    update

--- src/app/Search/search/search.component.ts
+++ src/app/Search/search/search.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import {SearchService} from '../../Service/search.service';
 import {LOCAL_STORAGE , WebStorageService} from 'angular-webstorage-service';
+import { AuthServiceService } from 'src/app/Auth/auth-service.service';
+import { BookmarkServices } from 'src/app/Service/bookmark-services.service';
 
 @Component({
   selector: 'app-search',
@@ -10,10 +12,25 @@ import {LOCAL_STORAGE , WebStorageService} from 'angular-webstorage-service';
 export class SearchComponent implements OnInit {
 show = false;
 results = [];
-  constructor(@Inject(LOCAL_STORAGE) public storage: WebStorageService, public search: SearchService) { }
+  constructor(@Inject(LOCAL_STORAGE) public storage: WebStorageService,private AuthService:AuthServiceService, public search: SearchService,private bookmarksService:BookmarkServices) { 
+   
+  }
   ngOnInit() {
     this.results = this.storage.get('searchResult');
-console.log(this.results.values = null);
+    this.bookmarksService.token = this.storage.get('token');
+    console.log(this.bookmarksService.token);
   }
+ 
+bookmark(id){
+console.log(id);
+this.bookmarksService.addProductBookmarks(id).subscribe(res=>{
+  console.log(res);
+})
+}
 
+serviceBookmark(id){
+this.bookmarksService.addServiceBookmark(id).subscribe(res=>{
+  console.log(res);
+})
+}
 }
---
+++ src/app/Service/bookmark-services.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from "@angular/core";
+import { Http , Headers} from "@angular/http";
+
+
+@Injectable({
+    providedIn:'root'
+})
+
+export class BookmarkServices{
+token;
+constructor(public http:Http){}
+addProductBookmarks(id){
+    const headers = new Headers()
+    headers.append('x-auth',this.token);
+    console.log(this.token);
+    return this.http.patch('http://localhost:3000/bookmark/product/' +id , id ,{headers: headers});
+}
+addCompanyBookmark(id){
+    const headers=new Headers();
+    headers.append('x-auth',this.token);
+    return this.http.patch('http//localhost:3000/bookmark/company/'+id,id,{headers:headers});
+}
+addPostBookmark(id){
+    const headers=new Headers();
+    headers.append('x-auth',this.token);
+    return this.http.patch('http//localhost:3000/bookmark/post/'+id,id,{headers:headers});
+}
+addServiceBookmark(id){
+    const headers=new Headers();
+    headers.append('x-auth',this.token);
+    return this.http.patch('http//localhost:3000/bookmark/service/'+id,id,{headers:headers});
+}
+
+}
