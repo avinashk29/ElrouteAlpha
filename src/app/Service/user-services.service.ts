04e75085b12724550962205526e8b0adf9748401
Arjun4396kumar <arjunkumar4396@gmail.com>
2019-02-21 12:03:32 +0530

    getting changes

--- src/app/Service/user-services.service.ts
+++ src/app/Service/user-services.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import {Http , Headers} from '@angular/http';
-import {  } from 'angular-webstorage-service';
+import {BehaviorSubject} from 'rxjs'
 
 @Injectable({
     providedIn: 'root'
 })
 export class UserService {
     token;
+    private empDetailSubject = new BehaviorSubject(null); 
     constructor(public http: Http) {}
 
     getUserData() {
@@ -20,13 +21,15 @@ editUser(User){
     headers.append('x-auth', this.token);
     console.log(this.token);
     const UserFormData = new FormData();
-    UserFormData.append('Image' , User.Image);
-    UserFormData.append('UserName' , User.UserName);
-    UserFormData.append('Image' , User.Title);
-    UserFormData.append('Image' , User.Image);
-    UserFormData.append('Image' , User.Image);
-    UserFormData.append('Image' , User.Image);
+    // UserFormData.append('Image' , User.Image);
+    // UserFormData.append('UserName' , User.UserName);
+    // UserFormData.append('Image' , User.Title);
+    // UserFormData.append('Image' , User.Image);
+    // UserFormData.append('Image' , User.Image);
+    // UserFormData.append('Image' , User.Image);
     return this.http.patch('http://localhost:8080/api/user', User, {headers: headers});
 }
+
+
 
 }
